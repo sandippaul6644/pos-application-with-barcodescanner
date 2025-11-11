6c637113e1292138b68d97da5688601a91fe1231
sandippaul6644 <sandippaul7318@gmail.com>
2025-11-12 02:10:33 +0530

    ls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  imageFolder: 'C:\\Users\\sandi\\Desktop\\image',
-  videoFolder: 'C:\\Users\\sandi\\Desktop\\video'
+  imageFolder: "C:\\Users\\sandi\\Desktop\\image",
+  videoFolder: "C:\\Users\\sandi\\Desktop\\video",
 };
